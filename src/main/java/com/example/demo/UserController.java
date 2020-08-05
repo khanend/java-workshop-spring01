c2175d60547237d323a3c3d8e28a7a27c30dff4f
Smarch Poonkwan <smarchpoo@mathregit.group>
2020-08-05 13:23:30 +0700

    request param query string

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,15 +1,29 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserController {
     @GetMapping("/users")
-    public   UsersResponse[] getAllUser(){
-        UsersResponse[] usersResponses = new  UsersResponse[2];
-        usersResponses[0] = new UsersResponse(1,"User 1");
-        usersResponses[1]= new UsersResponse(2,"User 2");
+    public   List<UsersResponse> getAllUser(){
+        List<UsersResponse> usersResponses = new ArrayList<>();
+        usersResponses.add(new UsersResponse(1,"User 1"));
+        usersResponses.add(new UsersResponse(2,"User 2"));
+        usersResponses.add(new UsersResponse(3,"User 3"));
         return usersResponses;
+    }
+//    @GetMapping("/users/{id}")
+//    public UsersResponse getUserById(@PathVariable int id){
+//        return new UsersResponse(id, "User"+id);
+//    }
+//
+    @GetMapping("/users/")
+    public  String  getUserByString(@RequestParam(required = false ,defaultValue = "2") String page ,
+                                    @RequestParam(required = false, defaultValue = "15" ) String item_per_page
+                                    ){
+            return   "Page: " + page ;
     }
 }
