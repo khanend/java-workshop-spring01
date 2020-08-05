f3864ad1a6f22cf75ee5ec4c3772aa45787cb62f
Smarch Poonkwan <smarchpoo@mathregit.group>
2020-08-05 11:02:38 +0700

    step-01 userResponse

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -6,7 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
     @GetMapping("/users")
-    public  String getAllUser(){
-        return "Get All User";
+    public   UsersResponse[] getAllUser(){
+        UsersResponse[] usersResponses = new  UsersResponse[2];
+        usersResponses[0] = new UsersResponse(1,"User 1");
+        usersResponses[1]= new UsersResponse(2,"User 2");
+        return usersResponses;
     }
 }
